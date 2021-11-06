c756b6dac13d3ff2ff17a160c5a443ee8da6780e
Alex Kavvos <alex.kavvos@bristol.ac.uk>
2021-11-06 17:47:44 +0000

    update compile script to fail on wrong name

--- compile.bat
+++ compile.bat
@@ -1,4 +1,8 @@
 @echo off
+if not exist "%~n1.tex" (
+  echo file %~n1.tex does not exist
+  exit 1
+)
 echo \PassOptionsToClass{answers}{exam} >"%~n1-answers.tex"
 type "%~n1.tex" >> "%~n1-answers.tex"
 latexmk "%~n1.tex" "%~n1-answers.tex"
